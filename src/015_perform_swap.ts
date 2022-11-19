58422020a6759f45f18599f2759d9805bafe2b94
Benjamin Pabst <benjamin.pabst@capgemini.com>
2022-11-19 21:58:59 +0100

    Code cleanup

--- src/015_perform_swap.ts
+++ src/015_perform_swap.ts
@@ -7,13 +7,9 @@ import secret from "../wallet.json";
 
 
 export async function swapSolWithOrca(_solAmount, callback) {
-  // Initialize a connection to the RPC and read in private key
   const connection = new Connection("https://api.devnet.solana.com", "singleGossip");
   const orca = getOrca(connection, Network.DEVNET);
   const keypair = Keypair.fromSecretKey(new Uint8Array(secret));
-//   console.log("endpoint:", connection.rpcEndpoint);
-//   console.log("wallet pubkey:", keypair.publicKey.toBase58());
-
   const orcaSolPool = orca.getPool(OrcaPoolConfig.ORCA_SOL);
   const solToken = orcaSolPool.getTokenB();
   const solAmount = new Decimal(_solAmount as Decimal.Value);
@@ -21,7 +17,7 @@ export async function swapSolWithOrca(_solAmount, callback) {
   const orcaAmount = quote.getMinOutputAmount();
 
   console.log(
-    `Swappin ${_solAmount.toString()} SOL for at least ${orcaAmount.toNumber()} ORCA...`
+    `Swapping ${_solAmount.toString()} SOL for at least ${orcaAmount.toNumber()} ORCA...`
   );
   const swapPayload = await orcaSolPool.swap(
     keypair,
@@ -30,6 +26,6 @@ export async function swapSolWithOrca(_solAmount, callback) {
     orcaAmount
   );
   const swapTxId = await swapPayload.execute();
-  console.log("Swapped:", swapTxId, "\n");
+  console.log("Swapped (TX:", swapTxId, ")");
   callback && callback();
 }
